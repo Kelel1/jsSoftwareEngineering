9c7a92c5eb682666d984711351dd88cf9a475eff
Kern Elder <kelel1@yahoo.com>
2019-01-10 22:13:15 -0500

    add Drink and Order classes

--- jambajuice1.js
+++ jambajuice1.js
@@ -10,6 +10,43 @@ class Menuitem {
   }
 }
 
+class Drink {
+
+  constructor(item, size) {
+
+    this.item = item;
+    this.size = size;
+    this.price = item.price[size];
+    this.calories = item.calories[size];
+  }
+}
+
+class Order {
+
+  constructor() {
+
+    this.orders = []
+  }
+
+  orderDrink(drink) {
+
+    this.orders.push(drink);
+  }
+
+  totalCost() {
+
+    let total = 0;
+    for(let drinks of this.orders) {
+      total += drinks.price;
+    }
+    return total;
+  }
+
+
+
+}
+
+
 class Menu {
 
   constructor() {
@@ -17,11 +54,14 @@ class Menu {
   this.menuItems = [];
   
   }
+
   
   addMenuItem(item) {
     
     this.menuItems.push(item);
   }
+
+  
   
   // my implementation
 /*    findMenuItems(ingredient) {
@@ -62,31 +102,38 @@ class Menu {
 
 
 const PapayaSunrise    = new Menuitem("PapayaSunrise", ingredients = ["papay", "strawberry", "peach"],
-                                   price = {small: "5.15", medium: "5.75", large: "6.55"},
+                                   price = {small: 5.15, medium: 5.75, large: 6.55},
                                   calories = {small: 190, medium: 280, large: 330});
 
 const PeachPerfection  = new Menuitem("PeachPerfection", ingredients = ["peach", "mango", "strawbery"],
-                                   price = {small: "5.15", medium: "5.75", large: "6.55"},
+                                   price = {small: 5.15, medium: 5.75, large: 6.55},
                                    calories = {small: 210, medium: 320, large: 360});
 
 const StrawberryDragon = new Menuitem("StrawberryDragon", ingredients = ["pitaya", "strawberry",
                                   "orange", "passionfruit", "mango", "banana"],
-                                  price = {small: "5.85", medium: "6.25", large: "6.85"},
+                                  price = {small: 5.85, medium: 6.25, large: 6.85},
                                   calories = {small: 360, medium: 480, large: 610});
 
 const StrawberryWhirl  = new Menuitem("StrawberryWhirl", ingredients = ["strawberry", "banana", "apple"],
-                                   price = {small: "5.15", medium: "5.75", large: "6.55"},
+                                   price = {small: 5.15, medium: 5.75, large: 6.55},
                                    calories = {small: 210, medium: 310, large: 380});
                                    
                                    
 let jamba = new Menu();
+let drink = new Drink(StrawberryDragon, "small");
+let drink2 = new Drink(StrawberryDragon, "large");
+let order2Go = new Order();
+order2Go.orderDrink(drink);
+order2Go.orderDrink(drink2);
+console.log(order2Go.totalCost());
 
 jamba.addMenuItem(StrawberryDragon);
 jamba.addMenuItem(StrawberryWhirl);
 jamba.addMenuItem(PapayaSunrise);
 
 /* console.log(jamba); */
-console.log(jamba.findMenuItems("mango")); 
+/* console.log(jamba.findMenuItems("mango")); */ 
+console.log(drink);
 
 
 
