9909f47738f8abfd072744cef2ddb066f24c3e73
Kern Elder <kelel1@yahoo.com>
2019-01-30 22:10:26 -0500

    more practice

--- undScore.js
+++ undScore.js
@@ -19,3 +19,36 @@ ppl = [1, 2, 3];
 // 6, 7, 8
 addFive(ppl);
 
+function concat(arr) {
+	
+    let newStr = "";
+      let l = _.reduce(arr, shrink = (mem, str) => {newStr += str},0);
+    console.log(newStr);
+}
+ppl = ["1", "2", "3"];
+// "123"
+concat(ppl);
+
+function longString(arr) {
+	
+  
+    return _.find(arr,length = (string) => {if(string.length > 10) {
+        console.log(string);
+  }});
+  
+}
+ppl = ["1", "20000000000000", "315"];
+// "123"
+longString(ppl);
+
+function longStrings(arr) {
+	
+  
+	return _.filter(arr,length = (string) => {if(string.length > 10) {
+  console.log(string);
+  }});
+  
+}
+ppl = ["100000000000", "20000000000000", "310000000000005"];
+// "123"
+longStrings(ppl);
