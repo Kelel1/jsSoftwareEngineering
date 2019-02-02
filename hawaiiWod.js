66259b86a69b1c537c1d73a13904b1ebcbea7e34
Kern Elder <kelel1@yahoo.com>
2019-02-01 23:01:05 -0500

    complete part1 wod

---
+++ hawaiiWod.js
@@ -0,0 +1,51 @@
+let testData = [
+    {
+      "FISCAL_YEAR":2010,
+      "CAMPUS":"UH Manoa",
+      "CIP":220101,
+      "CIP_DESC":"Law",
+      "GROUP1":"School of Law",
+      "GROUP2":"School of Law",
+      "GROUP3":"Law",
+      "GROUP4":"",
+      "GROUP5":"",
+      "OUTCOME":"Other",
+      "HAWAIIAN_LEGACY":"",
+      "AWARDS":69
+    },
+    {
+      "FISCAL_YEAR":2010,
+      "CAMPUS":"Kapi`olani CC",
+      "CIP":240101,
+      "CIP_DESC":"Liberal Arts",
+      "GROUP1":"General & Pre-Prof Ed",
+      "GROUP2":"Liberal Arts",
+      "GROUP3":"Liberal Arts",
+      "GROUP4":"",
+      "GROUP5":"",
+      "OUTCOME":"Associate Degrees",
+      "HAWAIIAN_LEGACY":"",
+      "AWARDS":322
+    }];
+  console.log(testData);
+  
+  
+  function totalDegrees(data) {
+  
+      let totlDegrees = _.reduce(data, function(iter,n){return iter += n.AWARDS}, 0);
+    
+    return totlDegrees;
+  }
+  
+  function percentageHawaiian(data) {
+  
+      //let size = _.size(data);
+    let hawaiiLegacy = _.filter(data, function(n){if(n['HAWAIIAN_LEGACY'] == 'HAWAIIAN') {  
+        return n;
+    }});
+    
+    
+      return ((totalDegrees(hawaiiLegacy)/ totalDegrees(data)) * 100);
+  }
+  
+  console.log(percentageHawaiian(uhdata));
